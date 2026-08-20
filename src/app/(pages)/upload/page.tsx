5380c7eba0e29c04eda2e0ef2771cf5e6058816a
Daniel Lorigan <dlorigan@uw.edu>
2026-08-20 14:54:02 -0700

    More type cleanups

--- src/app/(pages)/upload/page.tsx
+++ src/app/(pages)/upload/page.tsx
@@ -78,10 +78,16 @@ const Query: React.FC = () => {
 
   async function handleJSONFile(file: File) {
     setJSONError("");
-    async function parseJsonFile(file) {
+    async function parseJsonFile(file: File) {
       return new Promise((resolve, reject) => {
         const fileReader = new FileReader()
-        fileReader.onload = event => resolve(JSON.parse(event.target.result))
+        fileReader.onload = event => {
+          if (!(typeof event.target?.result === "string") || !event.target.result) {
+            reject("Failed to parse file");
+          }
+          resolve(JSON.parse(event.target?.result as string))
+        }
+
         fileReader.onerror = error => reject(error)
         fileReader.readAsText(file)
       })
--- src/app/backend/upload/service.ts
+++ src/app/backend/upload/service.ts
@@ -1,5 +1,4 @@
 "use server";
-import { Bundle, Resource } from "fhir/r4";
 
 import { auditable } from "../audit-logs/decorator";
 import {
@@ -12,8 +11,8 @@ class UploadService {
   @auditable
   static async postFHIRBundle(
     fhirServer: string,
-    bundle: Bundle
-  ): Promise<{ status: number; ok: boolean; body: Resource}> {
+    bundle: unknown
+  ): Promise<{ status: number; ok: boolean; body: unknown}> {
     const fhirClient = await prepareFhirClient(fhirServer);
   
     // Get the server config to check for mutual TLS
@@ -60,7 +59,7 @@ class UploadService {
    */
   private static async handleStandardDiscovery(
     fhirClient: FHIRClient,
-    bundle: Bundle,
+    bundle: unknown,
   ): Promise<Response> {
     const endpoint = "/";
     return await fhirClient.postJson(endpoint, bundle);
